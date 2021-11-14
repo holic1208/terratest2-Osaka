5979d0c17418cc312e93331657a5ef3d348b4e28
holic1208 <holic1208>
2021-11-15 00:31:08 +0900

    step1

---
+++ 00_ssh.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "suuu" {
+  key_name   = "suuu-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDF/XEETkYCiy/sm4unBgT86AZYI3XAI2OlCZJeC8P5s888kOXR2kB4TutV4H/oV+2/OVc0K3lnB/NRGQZRDcLvQMFT6qA1e1+s0lKk1cCMAkPWjXv1mC/Z4mMx3lV46WNPij0ZkG8U2bR/3jx5RrhAHR6sngbf+lYqwzoN1i02OEgwiP4u3ydBYF3uNdmKaed60TOaj69WjllVdTS/Ljs9tUiey5GPaPbnP6bi1b4GQYeDUv/Iv13mY3XoiBEh0RTd6weBB9OwRIkeeJa0etyeprDlwbcLdMOlLdJv1c9VegYkUI1Cv5VoWyEkCkTMN1lOccrgpYhE2+87zVREz9IqOAfCeDuaJ+0eFG54dojGxf6uy/bgHdK+W0rgPPXbJ7A3faApRGxagxq6zeGSmulZyaxNKRtz6AnJnMYelDU/kg6dzlPqGZTznzTO7Rnf7ZIfMjgjFTZxiWBRoNvDEnG/o/3cnmfFVhg5oYDeDn605RUZFC8cdgtnRyHj14DWzAs="
+}
